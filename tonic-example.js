d501144addb67aa91309da1b38373a03ffc0a141
Joseph Bryan <joemfb@gmail.com>
2016-06-03 01:39:07 -0400

    adds example for tonicdev.com

---
+++ tonic-example.js
@@ -0,0 +1,7 @@
+var nock = require('nock.js')
+
+// increment 1
+console.log(nock.nock(1, [4, 0, 1]))
+
+// decrement 10
+console.log(nock.nock("[8 [8 [1 0] [1 6 [5 [1 0] 0 6] [0 0] 8 [1 0] 8 [1 6 [5 [0 30] 4 0 6] [0 6] 9 2 [0 2] [4 0 6] 0 7] 9 2 0 1] 0 1] 9 2 [0 4] [7 [0 3] 1 10] 0 11]"))
